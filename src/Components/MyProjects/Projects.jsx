862f87c86f0ec789b214ed4c62dd0725cfc8f1fb
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-03-01 02:32:50 -0300

    Adding a project

--- src/Components/MyProjects/Projects.jsx
+++ src/Components/MyProjects/Projects.jsx
@@ -1,5 +1,11 @@
 export const projectsData = [
     {
+        "thumb":"https://raw.githubusercontent.com/tomchen/stack-icons/master/logos/react.svg",
+        "title":"Twitter UI",
+        "description":"Um clone do Twitter, feito em ReactJS + TypeScript",
+        "link":"https://twitter-ui-clone-seven.vercel.app/" 
+    },
+    {
         "thumb":"https://raw.githubusercontent.com/get-icon/geticon/master/icons/javascript.svg",
         "title":"CRUD-JS",
         "description":"Um CRUD realizado apenas com JS, HTML, CSS, sem nenhum tipo de framework, library ou database.",
